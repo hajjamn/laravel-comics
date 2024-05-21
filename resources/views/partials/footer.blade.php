360f02ed09ecea1dec41e4a407713ec06e9bcacb
hajjamn <hajjamn@gmail.com>
2024-05-21 20:40:23 +0200

    ADD: working links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,9 +2,9 @@
   <div class="container text-center text-white py-2">
     <div class="mt-5">
       <ul class="list-group-flush">
-        <li class="list-group-item"><h2>Contacts</h2></li>
-        <li class="list-group-item">Facebook</li>
-        <li class="list-group-item">Instagram</li>
+        <li class="list-group-item"><h2>Links</h2></li>
+        <li class="list-group-item"><a href="{{route('home')}}">Home</a></li>
+        <li class="list-group-item"><a href="{{route('comics')}}">Comics</a></li>
       </ul>
     </div>
   </div>
