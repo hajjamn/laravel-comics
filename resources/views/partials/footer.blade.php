21d4db356f44df4053c4bbd2b062aa8e16cfec48
hajjamn <hajjamn@gmail.com>
2024-05-21 20:20:22 +0200

    First commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="bg-dark">
+  <div class="container text-center text-white">
+    <div class="mt-5">
+      <ul class="list-group-flush">
+        <li class="list-group-item"><h2>Contacts</h2></li>
+        <li class="list-group-item">Facebook</li>
+        <li class="list-group-item">Instagram</li>
+      </ul>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header class="bg-dark">
+  <div class="container">
+    <div class="text-bg-white text-black">
+      LOGO
+    </div>
+  </div>
+</header>
