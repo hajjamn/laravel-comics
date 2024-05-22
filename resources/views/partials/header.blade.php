c5979cdbf58d87815d79a333f939e5b5a6a35be5
hajjamn <hajjamn@gmail.com>
2024-05-22 17:51:11 +0200

    ADD: started working on the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,23 @@
 <header class="bg-dark">
-  <div class="container">
-    <div class="py-3 text-bg-white text-white">
-      <h1>LOGO</h1>
+  <section class="smaller-header">
+    <div class="container">
+      <div class="row align-items-center justify-content-end gx-3">
+        <ul class="small-header-list">
+          <li class="small-header-list-item">
+            <p>DC POWER&#x2120; VISA&#xae;</p>
+          </li>
+          <li class="small-header-list-item">
+            <p>ADDITIONAL DC SITES&#x25BC;</p>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
+  </section>
+  <section class="main-header">
+    <div class="main-header container">
+      <div class="row">
+        WHITE RECTANGLE
+      </div>
+    </div>
+  </section>
 </header>
