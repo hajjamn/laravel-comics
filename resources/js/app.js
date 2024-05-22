18b874aee816fb475326a48053906981c3ec2167
hajjamn <hajjamn@gmail.com>
2024-05-22 16:53:43 +0200

    REFACTOR: boostrapp added via npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,4 +5,11 @@ import './bootstrap';
 
 import.meta.glob([
   '../img/**'
-])
+]);
+
+// resoures/js/app.js
+/* Import Bootstrap 5 */
+// Import our custom CSS
+import '~resources/scss/app.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
